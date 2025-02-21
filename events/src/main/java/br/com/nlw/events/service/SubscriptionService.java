19fe8e0f048295b3503c0c44e7d4aa8576c8e3d0
RaulBirlem <raul.birlem@gmail.com>
2025-02-20 22:15:44 -0300

    indication

--- events/src/main/java/br/com/nlw/events/service/SubscriptionService.java
+++ events/src/main/java/br/com/nlw/events/service/SubscriptionService.java
@@ -3,6 +3,7 @@ package br.com.nlw.events.service;
 import br.com.nlw.events.dto.SubscriptionResponse;
 import br.com.nlw.events.exception.SubscriptionConflictException;
 import br.com.nlw.events.exception.EventNotFoundException;
+import br.com.nlw.events.exception.UserIndicadorNotFoundException;
 import br.com.nlw.events.model.Event;
 import br.com.nlw.events.model.Subscription;
 import br.com.nlw.events.model.User;
@@ -24,7 +25,7 @@ public class SubscriptionService {
     @Autowired
     private SubscriptionRepo subRepo;
 
-    public SubscriptionResponse createNewSubscription(String eventName, User user){
+    public SubscriptionResponse createNewSubscription(String eventName, User user, Integer userId){
         //recuperar evento pelo nome
         Event evt = evtRepo.findByPrettyName(eventName);
         if (evt == null){
@@ -34,11 +35,15 @@ public class SubscriptionService {
         if(userRec == null) { // usuário não cadastrado
             userRec = userRepo.save(user);
         }
-
+        User indicador = userRepo.findById(userId).orElse(null);
+        if (indicador == null) {
+            throw new UserIndicadorNotFoundException("Usuário "+userId+" não existe!");
+        }
         Subscription subs = new Subscription();
 
         subs.setEvent(evt);
         subs.setSubscriber(userRec);
+        subs.setIndication(indicador);
 
         Subscription tmpSub = subRepo.findByEventAndSubscriber(evt,userRec);
         if (tmpSub != null){ //já tem inscrição
